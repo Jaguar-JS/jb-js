85e0cfe296c34e41039162c6b0ecd11dd3677573
Jaguar-JS <117010302+Jaguar-JS@users.noreply.github.com>
2022-11-04 07:09:13 +0200

    Stage 4/8: Help is on the way
    As in the previous stage, use the following word list: python, java, swift, javascript;
    Once the computer has chosen a word from the list, show its first three letters. Replace the hidden letters with hyphens (-).

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const input = require('sync-input')
-console.log("H A N G M A N")
 const worlds = ["python", "java", "swift", "javascript"]
-const computer = Math.floor(Math.random() * worlds.length);
-let answer = input("Guess the word: ");
-if (worlds[computer] === answer) {
-    console.log("You survived!")
+console.log("H A N G M A N")
+let rndWorld = getRandomElement(worlds);
+
+let answer = input(`Guess the word ${rndWorld.replace(/./g, (val, idx) => idx > 2 ? `-` : val)}: `);
+
+if ( rndWorld === answer) {
+ console.log("You survived!")
 } else {
     console.log("You lost!")
+}
+
+function getRandomElement(array) {
+    let index = Math.floor(Math.random() * array.length);
+    return array[index];
 }
