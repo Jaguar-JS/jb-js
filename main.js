2dc224d20ef66a1ea48c4d9e3b81baa6b142af18
Jaguar-JS <117010302+Jaguar-JS@users.noreply.github.com>
2022-11-04 07:15:23 +0200

    Stage 5/8: Keep trying
    Your game should work as follows:
    
    Players have exactly eight attempts to guess the word by entering letters one by one. Asking for a letter, print: Input a letter:. If a player uncovered all the letters, but some attempts are still left, the program must continue to ask for input until all the tries are exhausted;
    If the letter doesn't appear in the word, the computer takes one try away – even if a user has already suggested this letter – and prints That letter doesn't appear in the word.;
    If the letter does appear in the word but a user has already suggested this letter and it's open, no need to print any messages;
    If all attempts are exhausted, the game ends; the program shows a final message (Thanks for playing!). Otherwise, players can continue to input letters;
    We continue to stick with the word list from the previous stage: python, java, swift, javascript. It ensures that your program is tested reliably. Don't worry about javascript. Yes, it's longer than 8 characters, but we'll keep it in the list for consistency since we're not done with developing the game yet and for now there is no win or lose.

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-const input = require('sync-input')
-const worlds = ["python", "java", "swift", "javascript"]
-console.log("H A N G M A N")
-let rndWorld = getRandomElement(worlds);
-
-let answer = input(`Guess the word ${rndWorld.replace(/./g, (val, idx) => idx > 2 ? `-` : val)}: `);
-
-if ( rndWorld === answer) {
- console.log("You survived!")
-} else {
-    console.log("You lost!")
+String.prototype.replaceAt = function (index, replacement) {
+    if (index >= this.length) return this.valueOf();
+    return this.substring(0, index) + replacement + this.substring(index + 1);
 }
 
-function getRandomElement(array) {
+const input = require('sync-input')
+
+const worlds = ["python", "java", "swift", "javascript"]
+let worldRND = getRandomEl(worlds);
+let mask = '-'.repeat(worldRND.length)
+
+let attempts = 8;
+let letter = ''
+
+console.log(`H A N G M A N`)  // ${attempts} attempts
+game(attempts)
+console.log('\nThanks for playing!');
+
+function getRandomEl(array) {
     let index = Math.floor(Math.random() * array.length);
     return array[index];
+}
+
+function game(attempts) {
+    while (attempts--) {
+        letter = input(`\n${mask}\nInput a letter: `);
+        let ind = worldRND.indexOf(letter)
+
+        if (worldRND.includes(letter)) {
+            for (ind; ind < worldRND.length; ind++) {
+                if (worldRND[ind] === letter) mask = mask.replaceAt(ind, letter);
+            }
+        } else {
+            console.log(`That letter doesn't appear in the word.`)  // ${attempts} attempts
+        }
+    }
 }
