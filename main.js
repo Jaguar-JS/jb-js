d0b179b1deb46693313a691df875a65c5ed41d76
Jaguar-JS <117010302+Jaguar-JS@users.noreply.github.com>
2022-11-04 14:08:54 +0200

    Stage 8/8: Menu, please
    The game now starts with the menu where players can choose to either play, see their score, or exit;
    Print Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit: and show this message again if players input something else;
    If players choose play, start the game. When the game is finished (regardless of whether a player wins or not), pop up the menu once again to make the gameplay seamless;
    If players choose results, print how many games they won, e.g. You won: 0 times, and how many games they lost, e.g. You lost: 25 times;
    If players choose exit, end the game.

--- main.js
+++ main.js
@@ -5,25 +5,48 @@ String.prototype.replaceAt = function (index, replacement) {
 
 const input = require('sync-input')
 const worlds = ["python", "java", "swift", "javascript"]
-let worldRND = getRndEl(worlds);
-let mask = '-'.repeat(worldRND.length)
 
 let attempts = 8;
 let letter = '';
 let letterInputAll = '';
+const abc = new RegExp('[a-z]');
 
+let lostGame = 0;
+let wonGame = 0;
 
 console.log(`H A N G M A N`)  // ${attempts} attempts
-game(attempts)
+init();
 
-
-function getRndEl(array) {
-    let index = Math.floor(Math.random() * array.length);
-    return array[index];
+function init() {
+    let exit = true;
+    while (exit) {
+        let inputAction = input('Type \"play\" to play the game, \"results\" to show the scoreboard, and \"exit\" to quit: ');
+        switch (inputAction) {
+            case "play":
+                game(attempts);
+                break;
+            case "results":
+                console.log(`You won: ${wonGame} times.\nYou lost: ${lostGame} times.`)
+                break;
+            case "exit":
+            case "e":
+                console.log('Goodbye!');
+                exit = false;
+                break;
+            default:
+                console.log('Please, choose one of the possible actions');
+                break;
+        }
+    }
 }
 
+
 function game(attempts) {
-    
+    let worldRND = getRndEl(worlds);
+    let mask = '-'.repeat(worldRND.length);
+    letterInputAll = '';
+    // lostGame = 0;
+    // wonGame = 0;
     while (attempts--) {
         letter = input(`\n${mask}\nInput a letter: `);
 
@@ -33,19 +56,21 @@ function game(attempts) {
         }
 
         if (worldRND.includes(letter)) {
-
             for (let ind = worldRND.indexOf(letter); ind < worldRND.length; ind++) {
                 if (worldRND[ind] === letter) mask = mask.replaceAt(ind, letter);
             }
             attempts++;
         } else {
             console.log(`That letter doesn't appear in the word.  // ${attempts} attempts`)
-
         }
 
-        if (!attempts) console.log("You lost!");
+        if (!attempts) {
+            console.log("You lost!");
+            lostGame += 1;
+        }
         if (worldRND === mask) {
             console.log(`You guessed the word ${worldRND}!\nYou survived!`);
+            wonGame += 1;
             break;
         }
         letterInputAll += letter;
@@ -54,7 +79,6 @@ function game(attempts) {
 
 function validateLetter(letter) {
     const abc = new RegExp('[a-z]');
-    
     if (!letter || letter.length > 1) {
         console.log("Please, input a single letter.");
         return false;
@@ -72,4 +96,9 @@ function validateLetter(letter) {
     }
 
     return true;
+}
+
+function getRndEl(array) {
+    let index = Math.floor(Math.random() * array.length);
+    return array[index];
 }
