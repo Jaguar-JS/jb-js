00dd6ef26766e820ecb7f80f2bb9b8b4fb79f3e7
Jaguar-JS <117010302+Jaguar-JS@users.noreply.github.com>
2022-11-04 07:04:38 +0200

    Stage 2/8: Let’s play a game
    Ask players for a possible word;
    Print You survived! if users guess the word;
    Print You lost! if users fail

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const input = require('sync-input')
 console.log("H A N G M A N")
-console.log("The game will be available soon.")
+let answerOfSugar = input("Guess the word: ");
+if ("python" === answerOfSugar) {
+    console.log("You survived!")
+} else {
+    console.log("You lost!")
+}
