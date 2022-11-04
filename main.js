ef5b8d0591487526327077ab094f2dd335fbc5be
Jaguar-JS <117010302+Jaguar-JS@users.noreply.github.com>
2022-11-04 07:06:38 +0200

    Stage 3/8: Make your choice
    Create the following list of words: python, java, swift, javascript;
    Program the game to choose a word from the list at random (you can use the Math.random() method for that). You can enter more words, but let's stick to these four for now.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const input = require('sync-input')
 console.log("H A N G M A N")
-let answerOfSugar = input("Guess the word: ");
-if ("python" === answerOfSugar) {
+const worlds = ["python", "java", "swift", "javascript"]
+const computer = Math.floor(Math.random() * worlds.length);
+let answer = input("Guess the word: ");
+if (worlds[computer] === answer) {
     console.log("You survived!")
 } else {
     console.log("You lost!")
