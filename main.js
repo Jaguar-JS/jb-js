5b73ada4c258f01e727f7e758c2e213d45e73240
Jaguar-JS <117010302+Jaguar-JS@users.noreply.github.com>
2022-11-07 10:54:59 +0200

    Stage 2/6: Ingredient calculator
    Let's break the task into several steps:
    
    First, read the numbers of coffee drinks from the input.
    Figure out how much of each ingredient the machine will need. Note that one cup of coffee made on this coffee machine contains 200 ml of water, 50 ml of milk, and 15 g of coffee beans.
    Output the required ingredient amounts back to the user.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-console.log(`Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!`);
+const input = require('sync-input')
+
+const water=200
+const milk=50
+const coffeeBeans=15
+let countCups =input("Write how many cups of coffee you will need:\n");
+
+console.log(`For ${countCups} cups of coffee you will need:`);
+
+console.log(`${countCups*water} ml of water\n${countCups*milk} ml of milk\n${countCups*coffeeBeans} g of coffee beans`);
