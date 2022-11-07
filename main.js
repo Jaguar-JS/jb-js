142c1df3fa3082022f0a979059a38bbd54c0b411
Jaguar-JS <117010302+Jaguar-JS@users.noreply.github.com>
2022-11-07 11:02:00 +0200

    Stage 4/6: Buy, fill, take
    Write a program that offers to buy one cup of coffee or to fill the supplies or to take its money out. Note that the program is supposed to do one of the mentioned actions at a time. It should also calculate the amounts of remaining ingredients and how much money is left. Display the quantity of supplies before and after purchase.
    
    First, your program reads one option from the standard input, which can be "buy", "fill", "take". If a user wants to buy some coffee, the input is "buy". If a special worker thinks that it is time to fill out all the supplies for the coffee machine, the input line will be "fill". If another special worker decides that it is time to take out the money from the coffee machine, you'll get the input "take".
    If the user writes "buy" then they must choose one of three types of coffee that the coffee machine can make: espresso, latte, or cappuccino.
    For one espresso, the coffee machine needs 250 ml of water and 16 g of coffee beans. It costs $4.
    For a latte, the coffee machine needs 350 ml of water, 75 ml of milk, and 20 g of coffee beans. It costs $7.
    And for a cappuccino, the coffee machine needs 200 ml of water, 100 ml of milk, and 12 g of coffee beans. It costs $6.
    If the user writes "fill", the program should ask them how much water, milk, coffee and how many disposable cups they want to add into the coffee machine.
    If the user writes "take" the program should give all the money that it earned from selling coffee.
    At the moment, the coffee machine has $550, 400 ml of water, 540 ml of milk, 120 g of coffee beans, and 9 disposable cups.
    
    To sum up, your program should print the coffee machine's state, process one query from the user, as well as print the coffee machine's state after that. Try to use functions for implementing every action that the coffee machine can do

--- main.js
+++ main.js
@@ -1,34 +1,96 @@
 const input = require('sync-input')
-const oneCoffee = {
-    water: 200,
-    milk: 50,
-    coffeeBeans: 15
+const coffeeMachine = [
+    {
+        name: "The coffee machine has:",
+        water: 400,
+        milk: 540,
+        coffeeBeans: 120,
+        cups: 9,
+        money: 550
+    },
+    {
+        name: 'espresso',
+        water: 250,
+        milk: 0,
+        coffeeBeans: 16,
+        cups: 1,
+        money: 4
+    },
+
+    {
+        name: 'latte',
+        water: 350,
+        milk: 75,
+        coffeeBeans: 20,
+        cups: 1,
+        money: 7
+    },
+
+    {
+        name: 'cappuccino',
+        water: 200,
+        milk: 100,
+        coffeeBeans: 12,
+        cups: 1,
+        money: 6
+    },
+]
+
+const state = () => {
+    const {name, water, milk, coffeeBeans, cups, money} = coffeeMachine[0]
+    console.log()
+    console.log(`${name}`)
+    console.log(`${water} ml of water`)
+    console.log(`${milk} ml of milk`)
+    console.log(`${coffeeBeans} g of coffee beans`)
+    console.log(`${cups} disposable cups`)
+    console.log(`$${money} of money`)
+
 }
 
-const minCups = () => {
-    let inputWater = Number(input("Write how many ml of water the coffee machine has:\n"));
-    let inputMilk = Number(input("Write how many ml of milk the coffee machine has:\n"));
-    let inputCoffeeBeans = Number(input("Write how many grams of coffee beans the coffee machine has:\n"));
-    const {water, milk, coffeeBeans} = oneCoffee
-    const cupsWater = Math.floor(inputWater / water)
-    const cupsMilk = Math.floor(inputMilk / milk)
-    const cupsCoffeeBeans = Math.floor(inputCoffeeBeans / coffeeBeans)
+const buy = () => {
+    let inputVariant = Number(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n"));
+    console.log()
+    console.log(`${coffeeMachine[0].name}`)
+    console.log(`${coffeeMachine[0].water - coffeeMachine[inputVariant].water} ml of water`)
+    console.log(`${coffeeMachine[0].milk - coffeeMachine[inputVariant].milk} ml of milk`)
+    console.log(`${coffeeMachine[0].coffeeBeans - coffeeMachine[inputVariant].coffeeBeans} g of coffee beans`)
+    console.log(`${coffeeMachine[0].cups - coffeeMachine[inputVariant].cups} disposable cups`)
+    console.log(`$${coffeeMachine[0].money + coffeeMachine[inputVariant].money} of money`)
+}
+const fill = () => {
+    let inputWater = Number(input("Write how many ml of water you want to add:\n"));
+    let inputMilk = Number(input("Write how many ml of milk you want to add:\n"));
+    let inputCoffeeBeans = Number(input("Write how many grams of coffee beans you want to add:\n"));
+    let inputCups = Number(input("Write how many disposable coffee cups you want to add:\n"));
 
-    return Math.min(cupsWater, cupsMilk, cupsCoffeeBeans)
+    coffeeMachine[0].water += inputWater
+    coffeeMachine[0].milk += inputMilk
+    coffeeMachine[0].coffeeBeans += inputCoffeeBeans
+    coffeeMachine[0].cups += inputCups
+
+    state()
 }
 
-const cupsMachine = minCups()
-
-let inputCups = Number(input("Write how many cups of coffee you will need:\n"));
-
-if (cupsMachine < inputCups) {
-    // Нет, я могу сделать только ${} чашки кофе
-    console.log(`No, I can make only ${cupsMachine} cups of coffee`);
-} else if (inputCups === cupsMachine) {
-    // 'Да, я могу сделать столько кофе'
-    console.log('Yes, I can make that amount of coffee');
-} else {
-    // Да, я могу сделать такое количество кофе (и даже ${} больше этого)
-    console.log(`Yes, I can make that amount of coffee (and even ${cupsMachine - inputCups} more than that)`);
+const take = () => {
+    console.log(`I gave you $${coffeeMachine[0].money}`);
+    coffeeMachine[0].money = 0
+    state()
 }
 
+
+state()
+let inputAction = input("\nWrite action (buy, fill, take):\n");
+switch (inputAction) {
+    case 'buy':
+        buy()
+        break;
+    case 'fill':
+        fill()
+        break;
+    case 'take':
+        take()
+        break;
+    default:
+        break;
+}
