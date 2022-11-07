66b152d1755b0199696f2fa2636a119924d2ca43
Jaguar-JS <117010302+Jaguar-JS@users.noreply.github.com>
2022-11-07 02:38:30 +0200

    Stage 4/5: Can you repeat
    In this stage, your program should:
    
    Print the welcoming and greeting messages from the previous stage;
    Print the list of gifts;
    Handle initial input which the user chooses what to do;
    Handle the option to buy a gift and remove the gift from the list;
    Handle the option to add more tickets;
    Handle the option to see the total tickets;
    Handle the option to see the list of gifts;
    Handle a new option to quit the program;
    Run continuously;
    Terminate the program with the message.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const input = require('sync-input');
+
+let startTickets = 0;
 const gifts = [
     {id: 1, name: 'Teddy Bear', cost: 10},
     {id: 2, name: 'Big Red Ball', cost: 5},
@@ -11,42 +13,46 @@ const gifts = [
     {id: 9, name: 'Basketball', cost: 20},
     {id: 10, name: 'Scary Mask', cost: 75},
 ]
-let totalTickets = 100;
-
 
 console.log(`WELCOME TO THE CARNIVAL GIFT SHOP!
-Hello friend! Thank you for visiting the carnival!
-Here's the list of gifts:\n`);
+Hello friend! Thank you for visiting the carnival!`);
 printGifts(gifts);
 
-console.log('What do you want to do?');
-let inpAction = input('1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts\n');
-switch (inpAction) {
-    case '1':
-        let inpIdGift = parseInt(input('Enter the number of the gift you want to get: '));
-        const {id, name, cost} = gifts[inpIdGift - 1];
-        console.log(`Here you go, one ${name}!`);
-        console.log(`Total tickets: ${totalTickets - cost}`);
-        break;
-    case '2':
-        let amount = parseInt(input('Enter the ticket amount: '))
-        console.log(`Total tickets: ${totalTickets + amount}`);
-        break;
-    case '3':
-        console.log(`Total tickets: ${totalTickets }`);
-        break;
-    case '4':
-        console.log('Here\'s the list of gifts:\n');
-        printGifts(gifts)
-        break;
-    default:
-        console.log("Unknown input")
-        break;
+let exit = false;
+while (!exit) {
+    console.log('What do you want to do?');
+    let inpAction = input('1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts 5-Exit the shop\n');
+    switch (inpAction) {
+        case '1':
+            let inpIdGift = parseInt(input('Enter the number of the gift you want to get: '));
+            const {name, cost} = gifts[inpIdGift - 1];
+            console.log(`Here you go, one ${name}!`);
+            console.log(`Total tickets: ${startTickets - cost}\n`);
+            delete gifts[inpIdGift - 1];
+            break;
+        case '2':
+            let amount = parseInt(input('Enter the ticket amount: '))
+            console.log(`Total tickets: ${startTickets + amount}`);
+            break;
+        case '3':
+            console.log(`Total tickets: ${startTickets}`);
+            break;
+        case '4':
+            printGifts(gifts)
+            break;
+        case '5':
+            console.log('Have a nice day!');
+            exit = true;
+            break;
+        default:
+            console.log("Unknown input")
+            break;
 
+    }
 }
-console.log('Have a nice day!');
 
 function printGifts(gifts) {
+    console.log('Here\'s the list of gifts:\n');
     gifts.forEach(gift => {
         console.log(`${gift.id}- ${gift.name}, Cost: ${gift.cost} tickets`)
     })
